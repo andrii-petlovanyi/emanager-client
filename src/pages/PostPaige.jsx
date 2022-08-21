ae77c9a15ff832071958d855f52d39dc748cdd29
Andrii Petlovanyi <andrey.petlovany@gmail.com>
2022-08-21 20:08:13 +0300

    fullllllllll

--- src/pages/PostPaige.jsx
+++ src/pages/PostPaige.jsx
@@ -60,13 +60,13 @@ export const PostPage = () => {
                     
             <div className="post-item post-full-item">
                 <div className="post-header">
-                    <h3 className="post-title"><AiOutlineStar size={25} style={{ marginRight:'10px'}} />{post.model}</h3>
+                    <h3 className="post-title"><AiOutlineStar size={25} style={{ marginRight:'15px'}} />{post.model}</h3>
                     <Moment date={post.createdAt} format='D MMM YYYY' className="post-data"/>
                 </div>
                 <ul className="post-block">
-                    <li className="post-url"><span><AiOutlineCheck size={25} style={{ marginLeft: '10px', marginRight:'20px'}}/></span>{post.urlOff}</li>
-                    <li className="post-url"><span><AiOutlineRead size={25} style={{ marginLeft: '10px', marginRight:'20px'}}/></span>{post.urlBook}</li>
-                    <li className="post-url"><span><AiOutlineFileImage size={25} style={{ marginLeft: '10px', marginRight: '20px' }} /></span>{post.imgUrl}</li>
+                    <li className="post-url"><span><AiOutlineCheck size={25} style={{ marginLeft: '5px', marginRight:'15px'}}/></span>{post.urlOff}</li>
+                    <li className="post-url"><span><AiOutlineRead size={25} style={{ marginLeft: '5px', marginRight:'15px'}}/></span>{post.urlBook}</li>
+                    <li className="post-url"><span><AiOutlineFileImage size={25} style={{ marginLeft: '5px', marginRight: '15px' }} /></span>{post.imgUrl}</li>
                     <li className="post-text">{post.info}</li>
                 </ul>
             </div>       
