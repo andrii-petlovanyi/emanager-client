3b4bc34d11c0f6c6280f179d92f1abb28c92654b
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-10-31 20:05:05 +0200

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://91.231.182.50/api",
+  baseURL: "https://91.231.182.50/api",
 });
 
 instance.interceptors.request.use((config) => {
